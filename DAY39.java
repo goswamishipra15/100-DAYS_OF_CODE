05420eab813d4a95c2e6402ef58a4e43614881e2
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-09 17:53:56 +0530

    Two Sum IV - Input is a BST

---
+++ DAY39.java
@@ -0,0 +1,25 @@
+//Problem Statement URL: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashSet<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+        if(set.contains(root.val)) return true;
+        set.add(k-root.val);
+        return findTarget(root.left,k) || findTarget(root.right,k);
+    }
+}

1160b62b4b112c768d2092bc08ad036fadb36314
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-10 22:41:58 +0530

    Break a Palindrome

---
+++ DAY40.java
@@ -0,0 +1,15 @@
+//Problem Statement URL: https://leetcode.com/problems/break-a-palindrome/
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int len = palindrome.length();
+        char strArr[] = palindrome.toCharArray();
+        for(int i=0;i<len/2;i++){
+            if(strArr[i] != 'a'){
+                strArr[i] = 'a';
+                return String.valueOf(strArr);
+            }
+        }
+        strArr[len-1] = 'b';
+        return len<=1?"":String.valueOf(strArr);
+    }
+}

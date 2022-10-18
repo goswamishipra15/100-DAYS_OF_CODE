e83cb8031595b4f96c15be8dcc083cb286b5409c
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-18 23:14:43 +0530

    Add Digits

---
+++ DAY48.java
@@ -0,0 +1,8 @@
+//Problem Statement URL: https://leetcode.com/problems/add-digits/submissions/
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}

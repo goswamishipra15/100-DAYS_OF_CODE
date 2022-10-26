a566dba422bce0ed696546db5cd14d9a755d1d47
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-26 10:11:28 +0530

    Continuous Subarray Sum

---
+++ DAY56.java
@@ -0,0 +1,18 @@
+//Problem Statement URL: https://leetcode.com/problems/continuous-subarray-sum/
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hashMap = new HashMap<>(Map.of(0, 0));
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            // if the remainder sum % k occurs for the first time
+            if (!hashMap.containsKey(sum % k))
+                hashMap.put(sum % k, i + 1);
+            // if the subarray size is at least two
+            else if (hashMap.get(sum % k) < i)
+                return true;
+        }
+        return false;
+        
+    }
+}

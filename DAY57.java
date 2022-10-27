da5ac1c6a8d906748502f3218a6a38a4162f0225
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-27 15:50:47 +0530

    Shuffle String

---
+++ DAY57.java
@@ -0,0 +1,10 @@
+//Problem Statement URL:https://leetcode.com/problems/shuffle-string/
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char arr[] = new char[indices.length];
+        for(int i=0;i<indices.length;i++){
+            arr[indices[i]] = s.charAt(i);
+        }
+        return new String(arr);
+    }
+}
